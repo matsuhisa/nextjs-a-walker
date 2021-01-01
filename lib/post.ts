5734702dfd014bd8a5345230c6f5254eb49a4a10
matsuhisa <matsuhisa+github@gmail.com>
2021-01-02 00:36:38 +0900

    Add lib/post.ts

---
+++ lib/post.ts
@@ -0,0 +1,109 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
+import gfm from 'remark-gfm'
+const highlight = require('remark-highlight.js')
+
+const postsDirectory: string = path.join(process.cwd(), 'posts')
+
+export const getYearMonthPostIds = (year: string, month: string) => {
+  const ids: string[][] = []
+  const readDirectories = fs.readdirSync(`${postsDirectory}/${year}/${month}`, { withFileTypes: true })
+  readDirectories.forEach((result) => {
+    if (result.isFile()) {
+      ids.push([year, month, result.name.replace(/\.md$/, '')])
+    }
+  })
+  return ids
+}
+
+export const getAllPostIds = () => {
+  const fileNames = (dir: string, files: Array<String> = []) => {
+    const dirents = fs.readdirSync(dir, { withFileTypes: true })
+    const dirs = []
+    for (const dirent of dirents) {
+      if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`)
+      if (dirent.isFile()) files.push(`${dir}/${dirent.name}`.replace(path.join(process.cwd(), 'posts/'), ''))
+    }
+    for (const d of dirs) {
+      files = fileNames(d, files)
+    }
+    return files
+  }
+
+  return fileNames(postsDirectory).map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '').split('/'),
+      },
+    }
+  })
+}
+
+export const getAllYearMonths = () => {
+  const directories: { params: { year: string; month: string } }[] = []
+  const postDirectories = (directory: string) => {
+    const readDirectories = fs.readdirSync(directory, { withFileTypes: true })
+    readDirectories.forEach((result) => {
+      if (result.isDirectory()) {
+        const secondDirectories = fs.readdirSync(`${directory}/${result.name}`, { withFileTypes: true })
+        secondDirectories.forEach((result2) => {
+          if (result2.isDirectory()) {
+            directories.push({ params: { year: result.name, month: result2.name } })
+          }
+        })
+      }
+    })
+    return directories
+  }
+  return postDirectories(postsDirectory)
+}
+
+export const getAllYears = () => {
+  const directories: { params: { year: string } }[] = []
+  const postDirectories = (directory: string) => {
+    const readDirectories = fs.readdirSync(directory, { withFileTypes: true })
+    readDirectories.forEach((result) => {
+      if (result.isDirectory()) {
+        directories.push({ params: { year: result.name } })
+      }
+    })
+    return directories
+  }
+
+  return postDirectories(postsDirectory)
+}
+
+export const getPostData = async (id: string[] | any[]) => {
+  const fullPath = path.join(postsDirectory, `${id.join('/')}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const matterResult = matter(fileContents)
+
+  const processedContent = await remark().use(gfm).use(highlight).use(html).process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  }
+}
+
+export const getPostsData = async (ids: any[][]) => {
+  return ids.map((id: any[]) => {
+    const fullPath = path.join(postsDirectory, `${id.join('/')}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const matterResult = matter(fileContents)
+
+    const processedContent = remark().use(gfm).use(highlight).use(html).process(matterResult.content)
+    const contentHtml = processedContent.toString()
+
+    return {
+      id,
+      contentHtml,
+      ...matterResult.data,
+    }
+  })
+}
