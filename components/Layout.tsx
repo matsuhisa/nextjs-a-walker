767fa4f46d4756e47ec1a994bdada9bfe84497ea
matsuhisa <matsuhisa+github@gmail.com>
2021-01-02 00:21:34 +0900

    :wave: Remove files
    - 不要なファイルを削除

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'めも帖' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -16,24 +16,10 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </Head>
     <header>
       <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
     <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout>
+    <h1>めも帖</h1>
   </Layout>
 )
 
