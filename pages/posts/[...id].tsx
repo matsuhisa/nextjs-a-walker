960a765dcb8562b2a4ecbb6c7c1977eefeeebc56
matsuhisa <matsuhisa+github@gmail.com>
2021-01-02 00:54:15 +0900

    Use Layout

--- pages/posts/[...id].tsx
+++ pages/posts/[...id].tsx
@@ -1,16 +1,16 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
+import Layout from '../../components/Layout'
 import { Post } from '../../interfaces'
 import { getAllPostIds, getPostData } from '../../lib/post'
 
 const PostDetail = (data: Post) => {
-  const encodeTitle: string = encodeURIComponent(data.title)
+  const encodeTitle: string = encodeURIComponent(data.title.replace('/', '／'))
   return (
-    <>
+    <Layout>
       <h1>{data.title}</h1>
-      <img src={`https://res.cloudinary.com/kamonegi1977/image/upload/l_text:Sawarabi%20Gothic_50_bold:${encodeTitle},co_rgb:333,w_500,c_fit/v1608393336/ogp_iynadb.png`} />
-      <h2>本文です</h2>
       <div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
-    </>
+      <img src={`https://res.cloudinary.com/kamonegi1977/image/upload/l_text:Sawarabi%20Gothic_50_bold:${encodeTitle},co_rgb:333,w_500,c_fit/v1608393336/ogp_iynadb.png`} />
+    </Layout>
   )
 }
 export default PostDetail
