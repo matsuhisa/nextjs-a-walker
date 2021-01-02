99ee547b1cb4d0233713a7dd9c57774e91f13f25
matsuhisa <matsuhisa+github@gmail.com>
2021-01-03 00:59:27 +0900

    Use cloudinaryOgImageUrl

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,5 +2,6 @@ export type Post = {
   id: string[]
   title: string
   date: string
+  image?: string
   contentHtml: string
 }
--- pages/articles/[...id].tsx
+++ pages/articles/[...id].tsx
@@ -1,16 +1,22 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
+import Head from 'next/head'
 import Layout from '../../components/Layout'
 import { Post } from '../../interfaces'
+import { cloudinaryOgImageUrl } from '../../lib/cloudinaryOgImage'
 import { getAllPostPaths, getPostData } from '../../lib/post'
 
 const PostDetail = (data: Post) => {
-  const encodeTitle: string = encodeURIComponent(data.title.replace('/', '／'))
+  const ogImageUrl: string = data.image ? `/${data.image}`:cloudinaryOgImageUrl(data.title)
   return (
-    <Layout>
-      <h1>{data.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
-      <img src={`https://res.cloudinary.com/kamonegi1977/image/upload/l_text:Sawarabi%20Gothic_50_bold:${encodeTitle},co_rgb:333,w_500,c_fit/v1608393336/ogp_iynadb.png`} />
-    </Layout>
+    <>
+      <Head>
+        <meta property="og:image" content={ogImageUrl} key="title" />
+      </Head>
+      <Layout title={data.title}>
+        <h1>{data.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
+      </Layout>
+    </>
   )
 }
 export default PostDetail
