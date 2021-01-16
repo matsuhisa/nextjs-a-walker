02a35a6d354533ccfa3bfad8e895d817b1e80f18
matsuhisa <matsuhisa+github@gmail.com>
2021-01-17 00:10:59 +0900

    Add prev/next

--- pages/articles/[...id].tsx
+++ pages/articles/[...id].tsx
@@ -1,13 +1,16 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout from '../../components/Layout'
 import { Post } from '../../interfaces'
 import { cloudinaryOgImageUrl } from '../../lib/cloudinaryOgImage'
-import { getAllPostPaths, getAllYearMonths, getPostData } from '../../lib/post'
+import { getAllPostIds, getAllPostPaths, getAllYearMonths, getPostData, getPostsData } from '../../lib/post'
 
 type DetailProps = {
   post: Post,
   yearAndMonths?: any[],
+  nextPost: {},
+  prevPost: {},
 }
 
 const PostDetail = (data: DetailProps) => {
@@ -22,6 +25,24 @@ const PostDetail = (data: DetailProps) => {
           <h1 className={'post-title'}>{data.post.title}</h1>
           <div className={'post-date'}>{data.post.date}</div>
           <div className={'post-body'} dangerouslySetInnerHTML={{ __html: data.post.contentHtml }} />
+
+          <ul>
+            {data.nextPost && 
+              <li>
+                <Link href={`/articles/${data.nextPost.id.join('/')}`}>
+                  <a className={'post-pager__next'}>{data.nextPost.title}</a>
+                </Link>
+              </li>
+            }
+            {data.prevPost && 
+              <li>
+                <Link href={`/articles/${data.prevPost.id.join('/')}`}>
+                  <a className={'post-pager__next'}>{data.prevPost.title}</a>
+                </Link>
+              </li>
+            }
+          </ul>
+          <hr />
           <ul>
           {data.yearAndMonths?.map((yearAndMonth) => (
             <li>
@@ -49,9 +70,33 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     post = await getPostData(params?.id)
   }
 
+  const ids = getAllPostIds()
+  let posts = await getPostsData(ids)
+  posts = posts.sort((a,b) => {
+    if (a.unixtime < b.unixtime) {
+      return 1
+    } else {
+      return -1
+    }
+  })
+
+  const postids = posts.map( (post) => { return post.id.join('/') } )
+  const currentIndex: number = postids.indexOf(params?.id.join('/'))
+  let prevPost = null
+  let nextPost = null
+
+  if(currentIndex >= 0 && postids.length >= currentIndex){
+    prevPost = posts[currentIndex + 1]
+  }
+  if(currentIndex > 0){
+    nextPost = posts[currentIndex - 1]
+  }
+
   const data = {
     post: post,
     yearAndMonths: getAllYearMonths().map( (path) => { return path.params } ).sort((a, b) => Number(b.year) - Number(a.year)),
+    prevPost: prevPost,
+    nextPost: nextPost,
   }
 
   return { props: data }
